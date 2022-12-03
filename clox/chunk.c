96e6b777e8431c98d078f123f9a0f7d27e532dd9
Cody T. Zeitler <codyzeitler12@gmail.com>
2022-12-03 08:45:14 +0000

    feat: add chunk object

--- clox/chunk.c
+++ clox/chunk.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <stdint.h>
+#include "chunk.h"
+#include "memory.h"
 
-void helloWorld() {
-    printf("Hello, World!"); 
+void initChunk(Chunk* chunk) {
+    chunk->count = 0;
+    chunk->capacity = 0;
+    chunk->code = NULL;
+}
+
+void writeChunk(Chunk* chunk, uint8_t value) {
+    if (chunk->capacity < chunk->count + 1) {
+        int oldCapacity = chunk->capacity;
+        chunk->capacity = GROW_CAPACITY(oldCapacity);
+        chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+    }
+    chunk->code[chunk->count] = value;
+    chunk->count++;
+}
+
+void freeChunk(Chunk* chunk) {
+    FREE_ARRAY(uint8_t, chunk->code, chunk->count);
+    initChunk(chunk);
 }
