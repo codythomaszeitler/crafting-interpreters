b7ac9a2f087bd249b33a833eccffb87e08b11eaf
Cody T. Zeitler <codyzeitler12@gmail.com>
2022-12-11 05:39:43 +0000

    fix: add hash map

---
+++ clox/hashmap.h
@@ -0,0 +1,24 @@
+#ifndef HASHMAP_HEADER
+#define HASHMAP_HEADER
+
+#include "value.h"
+
+#define _NUM_HASH_BUCKETS_ 256
+
+typedef struct Entry Entry;
+struct Entry {
+    const char* key;
+    Value value;
+    Entry* next;
+};
+
+typedef struct HashMap {
+    Entry* entries[_NUM_HASH_BUCKETS_];
+} HashMap;
+
+void initHashMap(HashMap*);
+void freeHashMap(HashMap*);
+void hashMapPut(HashMap*, const char*, Value);
+Value hashMapGet(HashMap*, const char*);
+
+#endif
---
+++ clox/hashmap_test.c
@@ -0,0 +1,42 @@
+#include "unity.h"
+#include "hashmap.h"
+#include "value.h"
+
+HashMap testObject;
+void setUp()
+{
+    initHashMap(&testObject);
+}
+
+void tearDown()
+{
+    freeHashMap(&testObject);
+}
+
+void testItShouldPutValueInWithKey()
+{
+    hashMapPut(&testObject, "a", wrapNumber(5.0));
+    Value aValue = hashMapGet(&testObject, "a");
+    TEST_ASSERT_EQUAL(5.0, unwrapNumber(aValue));
+
+    hashMapPut(&testObject, "b", wrapNumber(6.0));
+    Value bValue = hashMapGet(&testObject, "b");
+    TEST_ASSERT_EQUAL(6.0, unwrapNumber(bValue));
+
+    hashMapPut(&testObject, "a", wrapNumber(3.0));
+    Value overriddenAValue = hashMapGet(&testObject, "a");
+    TEST_ASSERT_EQUAL(3.0, unwrapNumber(overriddenAValue));
+}
+
+void testItShouldGetNilIfNoKeyExists() {
+    Value nil = hashMapGet(&testObject, "a");
+    TEST_ASSERT_TRUE(isNil(nil));
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(testItShouldPutValueInWithKey);
+    RUN_TEST(testItShouldGetNilIfNoKeyExists);
+    return UNITY_END();
+}
